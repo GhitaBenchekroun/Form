4d5eb2d4b8a9d413c04c7a3ee906cedbe49d4514
GhitaBenchekroun <ghita.ben201@gmail.com>
2024-06-05 11:42:29 +0100

    form update

--- src/components/Form.js
+++ src/components/Form.js
@@ -67,6 +67,8 @@ function Form() {
   function handleChange(e) {
     const { name, value } = e.target;
     const re = /^[0-9\b]+$/;
+    const phoneRegex = /^(07|06)\d{8}$|^212\d{9}$|^\+212\d{9}$/
+
 
     if (name === "email") {
       console.log("heloo");
@@ -82,20 +84,32 @@ function Form() {
         setMessage("");
       }
     } else if (name === "Phone") {
-      const cleanedValue = value.replace(/\s+/g, "");
-      console.log("cleanedValue", cleanedValue);
-      const isNumeric = re.test(cleanedValue);
-      const isValidLength =
-        cleanedValue.length === 10 || cleanedValue.length === 12;
+      // const cleanedValue = value.replace(/\s+/g, "");
+      // console.log("cleanedValue", cleanedValue);
+      // const isNumeric = re.test(cleanedValue);
+      // const isValidLength =
+      //   cleanedValue.length === 10 || cleanedValue.length === 12;
 
-      // Set the error message if the value is not numeric or not the right length
-      if (!isNumeric) {
-        setPhoneError("Phone number must contain only numeric values.");
-      } else if (!isValidLength) {
-        setPhoneError("Phone number must have 10 or 12 digits.");
-      } else {
-        setPhoneError(""); // Clear error message when the phone is valid
-      }
+      // // Set the error message if the value is not numeric or not the right length
+      // if (!isNumeric) {
+      //   setPhoneError("Phone number must contain only numeric values.");
+      // } else if (!isValidLength) {
+      //   setPhoneError("Phone number must have 10 or 12 digits.");
+      // } else {
+      //   setPhoneError(""); // Clear error message when the phone is valid
+      // }
+      const cleanedValue = value.replace(/\s+/g, "");
+      const validPhone = phoneRegex.test(cleanedValue);
+
+    //   const cleanedValue = value.replace(/\s+/g, "");
+    // const isNumeric = re.test(cleanedValue);
+
+    // Set the error message if the value is not numeric or not matching the regex pattern
+    if (!validPhone) {
+      setPhoneError("Invalid phone format.");
+    } else {
+      setPhoneError(""); // Clear error message when the phone is valid
+    }
     }
     if (name === "yearOfExperience__c") {
       const cleanedValue = value.replace(/\s+/g, "");
@@ -444,6 +458,22 @@ function Form() {
     }));
   };
 
+
+  const deleteSkill = (index) => {
+    setLead((prevLead) => {
+      const updatedSkills = prevLead.Skills.filter((_, i) => i !== index);
+      return { ...prevLead, Skills: updatedSkills };
+    });
+  };
+  
+  const deleteCertification = (index) => {
+    setLead((prevLead) => {
+      const updatedCertifications = prevLead.Certifications.filter((_, i) => i !== index);
+      return { ...prevLead, Certifications: updatedCertifications };
+    });
+  };
+  
+
   return (
     // bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')]
     // bg-gradient-to-r from-cyan-500 to-blue-500
@@ -662,11 +692,11 @@ function Form() {
                   />
                 </label>
                 <div className=" ml-4 font-semibold">
-                  {fileName && <p>Selected file: {fileName}</p>}
+                  {fileName && <p>{fileName}</p>}
                 </div>
 
                 <button
-                  className="bg-niagara-600 text-white font-semibold py-2 px-4 rounded-full mx-8 ms-40 enabled:hover:border-gray-400 disabled:opacity-75"
+                  className="bg-niagara-600 text-white font-semibold py-2 px-4 rounded-full  ms-20 enabled:hover:border-gray-400 disabled:opacity-75"
                   type="type"
                   style={{
                     backgroundColor: "#05A88B",
@@ -715,7 +745,7 @@ function Form() {
               </div>
 
               <div>
-                <div className="flex flex-wrap -mx-3 mb-2 ">
+                <div className="flex  flex-wrap -mx-3 mb-2 ">
                   {Object.entries(lead).map(([key, values]) => {
                     // Check if the key is 'skills' to conditionally render the select box
                     if (key === "Skills") {
@@ -724,12 +754,12 @@ function Form() {
                           {values.map((skill, index) => (
                             <div
                               key={`${key}-${index}`}
-                              className="w-full md:w-1/2 px-3 mb-6 md:mb-0 mt-6"
+                              className="w-full md:w-1/2 px-3 mb-6 md:mb-0  mt-6"
                             >
                               <label className="block mb-2 ml-2 text-sm font-medium text-gray-900 dark:text-white">
                                 Skill
                               </label>
-                              <div className="flex center mb-2">
+                              <div className="flex center  mb-2">
                                 <input
                                   type="text"
                                   value={skill.name}
@@ -738,7 +768,7 @@ function Form() {
                                   }
                                   className="bg-gray-50 border mb-2 ml-2 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 />
-                                <div className="flex w-72 flex-col gap-6">
+                                <div className="flex w-72 flex-col  gap-6">
                                   <select
                                     value={skill.type}
                                     onChange={(e) =>
@@ -772,6 +802,44 @@ function Form() {
                                     ))}
                                   </select>
                                 </div>
+                                {/* <button
+          type="button"
+          onClick={() => deleteSkill(index)}
+          className="ml-2 bg-red-500 text-white font-bold py-1 px-2 rounded"
+        >
+          Delete
+        </button> */}
+<div className="flex w-36 bflex-col ">
+<button class="deleteButton"
+ type="button"
+ onClick={() => deleteSkill(index)}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 50 59"
+    class="bin"
+  >
+    <path
+      fill="#B5BAC1"
+      d="M0 7.5C0 5.01472 2.01472 3 4.5 3H45.5C47.9853 3 50 5.01472 50 7.5V7.5C50 8.32843 49.3284 9 48.5 9H1.5C0.671571 9 0 8.32843 0 7.5V7.5Z"
+    ></path>
+    <path
+      fill="#B5BAC1"
+      d="M17 3C17 1.34315 18.3431 0 20 0H29.3125C30.9694 0 32.3125 1.34315 32.3125 3V3H17V3Z"
+    ></path>
+    <path
+      fill="#B5BAC1"
+      d="M2.18565 18.0974C2.08466 15.821 3.903 13.9202 6.18172 13.9202H43.8189C46.0976 13.9202 47.916 15.821 47.815 18.0975L46.1699 55.1775C46.0751 57.3155 44.314 59.0002 42.1739 59.0002H7.8268C5.68661 59.0002 3.92559 57.3155 3.83073 55.1775L2.18565 18.0974ZM18.0003 49.5402C16.6196 49.5402 15.5003 48.4209 15.5003 47.0402V24.9602C15.5003 23.5795 16.6196 22.4602 18.0003 22.4602C19.381 22.4602 20.5003 23.5795 20.5003 24.9602V47.0402C20.5003 48.4209 19.381 49.5402 18.0003 49.5402ZM29.5003 47.0402C29.5003 48.4209 30.6196 49.5402 32.0003 49.5402C33.381 49.5402 34.5003 48.4209 34.5003 47.0402V24.9602C34.5003 23.5795 33.381 22.4602 32.0003 22.4602C30.6196 22.4602 29.5003 23.5795 29.5003 24.9602V47.0402Z"
+      clip-rule="evenodd"
+      fill-rule="evenodd"
+    ></path>
+    <path fill="#B5BAC1" d="M2 13H48L47.6742 21.28H2.32031L2 13Z"></path>
+  </svg>
+
+  <span class="tooltip">Delete</span>
+</button>
+</div>
+
                               </div>
                             </div>
                           ))}
@@ -850,7 +918,38 @@ function Form() {
                                     </option>
                                   ))}
                                 </select>
+                                <div className="flex w-36 bflex-col ">
+<button class="deleteButton"
+  type="button"
+  onClick={() => deleteCertification(index)}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 50 59"
+    class="bin"
+  >
+    <path
+      fill="#B5BAC1"
+      d="M0 7.5C0 5.01472 2.01472 3 4.5 3H45.5C47.9853 3 50 5.01472 50 7.5V7.5C50 8.32843 49.3284 9 48.5 9H1.5C0.671571 9 0 8.32843 0 7.5V7.5Z"
+    ></path>
+    <path
+      fill="#B5BAC1"
+      d="M17 3C17 1.34315 18.3431 0 20 0H29.3125C30.9694 0 32.3125 1.34315 32.3125 3V3H17V3Z"
+    ></path>
+    <path
+      fill="#B5BAC1"
+      d="M2.18565 18.0974C2.08466 15.821 3.903 13.9202 6.18172 13.9202H43.8189C46.0976 13.9202 47.916 15.821 47.815 18.0975L46.1699 55.1775C46.0751 57.3155 44.314 59.0002 42.1739 59.0002H7.8268C5.68661 59.0002 3.92559 57.3155 3.83073 55.1775L2.18565 18.0974ZM18.0003 49.5402C16.6196 49.5402 15.5003 48.4209 15.5003 47.0402V24.9602C15.5003 23.5795 16.6196 22.4602 18.0003 22.4602C19.381 22.4602 20.5003 23.5795 20.5003 24.9602V47.0402C20.5003 48.4209 19.381 49.5402 18.0003 49.5402ZM29.5003 47.0402C29.5003 48.4209 30.6196 49.5402 32.0003 49.5402C33.381 49.5402 34.5003 48.4209 34.5003 47.0402V24.9602C34.5003 23.5795 33.381 22.4602 32.0003 22.4602C30.6196 22.4602 29.5003 23.5795 29.5003 24.9602V47.0402Z"
+      clip-rule="evenodd"
+      fill-rule="evenodd"
+    ></path>
+    <path fill="#B5BAC1" d="M2 13H48L47.6742 21.28H2.32031L2 13Z"></path>
+  </svg>
+
+  <span class="tooltip">Delete</span>
+</button>
+</div>
                               </div>
+                              
                             </div>
                           ))}
 
